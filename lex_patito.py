2068f885bc8b8c8602e5fbad857ac0b0a99ce1d3
Claudia Arcienega <126215454+carcienegam@users.noreply.github.com>
2025-11-22 16:19:25 -0600

    lex (again)

--- lex_patito.py
+++ lex_patito.py
@@ -1,84 +1,105 @@
-import re
 import ply.lex as lex
 
 # Palabras reservadas
 reserved = {
-    'program': 'PROGRAM',
-    'main': 'MAIN',
-    'end': 'END',
-    'var': 'VAR',
-    'int': 'INT',
-    'float': 'FLOAT',
-    'void': 'VOID',
-    'print': 'PRINT',
-    'while': 'WHILE',
-    'do': 'DO',
-    'if': 'IF',
-    'else': 'ELSE',
+    'programa' : 'PROGRAMA',
+    'inicio'   : 'INICIO',
+    'fin'      : 'FIN',
+    'vars'     : 'VARS',
+    'entero'   : 'ENTERO',
+    'flotante' : 'FLOTANTE',
+    'nula'     : 'NULA',
+    'escribe'  : 'ESCRIBE',
+    'mientras' : 'MIENTRAS',
+    'haz'      : 'HAZ',
+    'si'       : 'SI',
+    'sino'     : 'SINO',
 }
 
-# Tokens
+#Lista de tokens
 tokens = [
-    'ID', 'CTE_INT', 'CTE_FLOAT', 'CTE_STRING', 
-    'SEMI', 'LBRACE', 'RBRACE', 'ASSIGN', 
-    'GTHAN', 'LTHAN', 'NOTEQ', 'LPAREN', 'RPAREN', 
-    'COMMA', 'COLON', 'LBRACKET', 'RBRACKET', 'PLUS', 'MINUS', 'MULT', 'DIV',
+    'ID',
+    'CTE_ENT',
+    'CTE_FLOAT',
+    'LETRERO',
+
+    # Operadores
+    'PLUS',
+    'MINUS',
+    'MULT',
+    'DIVIDE',
+    'GREATERTHAN',
+    'LESSTHAN',
+    'EQUAL',
+    'NOTEQ',
+    'ASSIGN',
+
+    # Simbolos
+    'SEMICOLON',
+    'COMMA',
+    'COLON',
+    'LEFTBRACE',
+    'RIGHTBRACE',
+    'LEFTPAREN',
+    'RIGHTPAREN',
+    'LEFTBRACKET',
+    'RIGHTBRACKET',
 ] + list(reserved.values())
 
-# Expresiones regulares simples
-t_SEMI      = r';'
-t_LBRACE    = r'\{'
-t_RBRACE    = r'\}'
-t_ASSIGN    = r'='
-t_GTHAN     = r'>'
-t_LTHAN     = r'<'
-t_NOTEQ       = r'!='
-t_LPAREN    = r'\('
-t_RPAREN   = r'\)'
-t_COMMA     = r','
-t_COLON     = r':'
-t_LBRACKET  = r'\['
-t_RBRACKET  = r'\]'
-t_PLUS      = r'\+'
-t_MINUS     = r'-'
-t_MULT      = r'\*'
-t_DIV       = r'/'
+# Reglas de expresiones regulares para tokens simples
 
-# ER numeros y strings
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_MULT = r'\*'
+t_DIVIDE = r'/'
+t_GREATERTHAN = r'>'
+t_LESSTHAN = r'<'
+t_EQUAL = r'=='
+t_NOTEQ = r'!='
+t_ASSIGN = r'='
+
+t_SEMICOLON = r';'
+t_COMMA = r','
+t_COLON = r':'
+t_LEFTBRACE = r'\{'
+t_RIGHTBRACE = r'\}'
+t_LEFTPAREN = r'\('
+t_RIGHTPAREN = r'\)'
+t_LEFTBRACKET = r'\['
+t_RIGHTBRACKET = r'\]'
+
+t_ignored = ' \t'
+
+def t_COMMENT(t):
+    r'//.*'
+    pass
+
 def t_CTE_FLOAT(t):
     r'-?\d*\.\d+'
     t.value = float(t.value)
     return t
 
-def t_CTE_INT(t):
+def t_CTE_ENT(t):
     r'-?\d+'
     t.value = int(t.value)
     return t
 
-def t_CTE_STRING(t):
+def t_LETRETO(t): #strings
     r'"[^"\n]*"'
     t.value = t.value[1:-1]
     return t
 
 def t_ID(t):
     r'[a-zA-Z_][a-zA-Z0-9_]*'
-    t.type = reserved.get(t.value, 'ID')
+    t.type = reserved.get(t.value, 'ID') 
     return t
-
-# Ignorar espacios y tabs, comentarios
-
-def t_comment(t):
-    r'\/\/[^\n]*'
-    pass
-
-t_ignore = ' \t\r\f\v'
 
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
 def t_error(t):
-    raise SyntaxError(f"Illegal character '{t.value[0]}' at line {t.lineno}")
+    print(f"Illegal character '{t.value[0]}' at line {t.lexer.lineno}")
+    t.lexer.skip(1)
 
-def build_lexer(**kwargs):
-    return lex.lex(**kwargs)
+lexer = lex.lex()
